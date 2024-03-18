8bea4b092cca1ea07fc6c66e112b3e90ba2908da
Álvaro Pimentel <alvaropimentel124@gmail.com>
2024-03-18 19:01:43 +0100

    mockup

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,39 +12,29 @@
 
         <div class="container-fluid">
             <div class="row">
-                <div class="col-12">
+                <div class="col-3 bg-light border rounded-2">
+                </div>
+                <div class="col-9">
                 <div class="row mx-5 my-2 p-3 bg-light border rounded-2">
                             <div class="col-4 flex-fill">
                                 <div class="product-image-container">
-                                    <img class="product-image" src="{{ asset('storage/uploads/' . basename($product->image_url)) }}" alt="{{ $product->name }}">
+                                    <img class="product-image" src="https://www.coope.com/wp-content/uploads/2018/09/logo-cooperativa-hosteleria-1.jpg">
                                 </div>
                             </div>
                             <div class="col-8 flex-fill">
                                 <h3>
 
-                                    <a href="{{ route('products.show', ['id' => $product->id]) }}">
-                                        {{ $product->name }}
-                                    </a>
+                                    <p>
+                                        Nombre del producto
+                                    </p>
                                 </h3>
-                                <h5>Vendedor: {{ $product->vendor_name }}</h5>
-                                <p class="text-wrap">{{ $product->description }}</p>
-                                <p>{{ $product->price }} €</p>
-                                @auth
-                                    
+                                <h5>Vendedor: Vendedor</h5>
+                                <p class="text-wrap">Descripcion del producto</p>
+                                <p>5€</p>
                                 
-                                @if (Auth::user()->role_id == 1)
-                                    @if (Route::has('carrito'))
-                                        <form action="{{ route('carrito.addToCart', $product->product_stockId) }}"
-                                            method="POST">
-                                            @csrf
                                             <button type="submit" class="btn btn-primary">Añadir al carrito</button>
-                                        </form>
-                                    @endif
-                                @endif
-                                @if (Auth::user()->role_id == 2)
-                                    <button type="button" class="btn btn-primary btn-sm open-modal" data-toggle="modal" data-target="#myModal" data-id="{{ $product->id }}">Añadir stock</button>
-                                @endif
-                                @endauth
+
+
 
                             </div>
                         </div>
